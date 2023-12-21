80b4c5c1dd03ab6cd39b6b2da0bb7daf16e425fa
Tatiana Kuznetsova <tatiana_kuznetsova@epam.com>
2023-12-21 17:37:17 +0600

    update cart models

--- src/cart/models/index.ts
+++ src/cart/models/index.ts
@@ -1,17 +1,25 @@
+enum CartStatuses {
+  OPEN = 'OPEN',
+  STATUS = 'STATUS',
+}
+
 export type Product = {
-  id: string,
-  title: string,
-  description: string,
-  price: number,
+  id: string;
+  title: string;
+  description: string;
+  price: number;
 };
 
-
 export type CartItem = {
-  product: Product,
-  count: number,
-}
+  product: Product;
+  count: number;
+};
 
 export type Cart = {
-  id: string,
-  items: CartItem[],
-}
+  id: string;
+  user_id: string;
+  created_at: string;
+  updated_at: string;
+  status: CartStatuses;
+  items: CartItem[];
+};
