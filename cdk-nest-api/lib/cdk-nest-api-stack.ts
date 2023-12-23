8fec83a0944e9180a9aaa02434a7c6ccf6bcdc4b
Tatiana Kuznetsova <tatiana_kuznetsova@epam.com>
2023-12-23 13:39:32 +0600

    add env variables to lambda

--- cdk-nest-api/lib/cdk-nest-api-stack.ts
+++ cdk-nest-api/lib/cdk-nest-api-stack.ts
@@ -2,6 +2,10 @@ import * as cdk from 'aws-cdk-lib';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Code, Runtime, Function } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import 'dotenv/config';
+
+const { RDS_HOSTNAME, RDS_PORT, RDS_USERNAME, RDS_PASSWORD, RDS_DB_NAME } =
+  process.env;
 
 export class CdkNestApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,6 +15,13 @@ export class CdkNestApiStack extends cdk.Stack {
       runtime: Runtime.NODEJS_18_X,
       handler: 'main.handler',
       code: Code.fromAsset('dist'),
+      environment: {
+        RDS_HOSTNAME: RDS_HOSTNAME!,
+        RDS_PORT: RDS_PORT!,
+        RDS_USERNAME: RDS_USERNAME!,
+        RDS_PASSWORD: RDS_PASSWORD!,
+        RDS_DB_NAME: RDS_DB_NAME!,
+      },
     });
 
     const restApi = new RestApi(this, 'CustomerLambdaApi', {
